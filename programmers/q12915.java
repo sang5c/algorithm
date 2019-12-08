0d5bf303eae9de81432380e03b413445ee84f1ac
박상오 <skah321@douzone.com>
2019-12-08 14:10:07 +0900

    programmers 12915

---
+++ programmers/q12915.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+	public String[] solution(String[] strings, int n) {
+		String[] answer;
+		Arrays.sort(strings);
+
+		List<String> temp = new ArrayList<>(Arrays.asList(strings));
+		List<String> temp2 = new ArrayList<>();
+
+		for (char a = 'a'; a <= 'z'; a++) {
+			for (String t : strings) {
+				if (t.charAt(n) == a) {
+					temp2.add(t);
+					temp.remove(t);
+				}
+			}
+		}
+		answer = temp2.toArray(new String[0]);
+		return answer;
+	}
+}
