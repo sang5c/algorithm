d73e6f433dda818bc957ab8cef70ce5b4461bc9c
sang5c <skah321@gmail.com>
2020-09-04 21:56:13 +0900

    add leetcode q7 - reverse integer

--- source/leetcode/easy/Q1_two_sum.java
+++ source/leetcode/easy/Q1_two_sum.java
@@ -1,11 +1,12 @@
-package leetcode;
+package leetcode.easy;
 
 import java.util.Arrays;
 
 /**
  * https://leetcode.com/problems/two-sum/
+ * Date: 2020-09-04
  */
-class Solution {
+public class Q1_two_sum {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -20,7 +21,7 @@ class Solution {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-        Solution solution = new Solution();
+        Q1_two_sum solution = new Q1_two_sum();
         int[] result = solution.twoSum(nums, target);
         System.out.println(Arrays.toString(result));
     }
---
+++ source/leetcode/easy/Q7_reverse_integer.java
@@ -0,0 +1,34 @@
+package leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/reverse-integer/
+ * Date: 2020-09-04
+ */
+public class Q7_reverse_integer {
+
+    // TODO: 문자열로 변환하지 않고 풀기
+    public int reverse(int x) {
+        int temp = x > 0 ? x : -x;
+
+        StringBuilder reverseBuilder = new StringBuilder("0");
+
+        while (temp > 0) {
+            reverseBuilder.append(temp % 10);
+            temp = temp / 10;
+        }
+        try {
+            int result = Integer.parseInt(reverseBuilder.toString());
+            return x > 0 ? result : -result;
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Q7_reverse_integer solution7 = new Q7_reverse_integer();
+        int result = solution7.reverse(1534236469);
+        System.out.println(result);
+    }
+
+}
