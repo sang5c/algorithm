ad0d137cffcdb9042f2483ca9412ade7b60f0273
sang5c <skah321@gmail.com>
2021-04-21 17:44:19 +0900

    add baekjoon 11399

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 - [leetcode](https://leetcode.com/)
 
 ## 한글
-- [programmers](https://programmers.co.kr/)
+- [프로그래머스](https://programmers.co.kr/)
+- [백준](https://www.acmicpc.net/) 
+
+## 기타
+- 백준 제출시 클래스명을 Main으로 지정해야 한다.
---
+++ source/baekjoon/q11399.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * https://www.acmicpc.net/problem/11399
+ * Title: ATM
+ * Date: 2021-04-21
+ * Type: greedy
+ */
+public class q11399 {
+    public static void main(String[] args) {
+        int timeSum = 0;
+        int timeWait = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        int count = Integer.parseInt(scanner.nextLine());
+        // int count = 5;
+        String times = scanner.nextLine();
+        // String times = "3 1 4 3 2";
+        List<Integer> timeSplit = Arrays.stream(times.split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        while (timeSplit.size() > 0) {
+            int min = 1000;
+            for (Integer i : timeSplit) {
+                if (i < min) {
+                    min = i;
+                }
+            }
+            timeWait += min;
+            timeSum += timeWait;
+            timeSplit.remove(Integer.valueOf(min));
+        }
+        System.out.println(timeSum);
+    }
+}
